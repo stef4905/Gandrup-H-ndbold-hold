4f1536f0e38a58b2a8077849069e5adaeb9e297a
stef4905 <wow99hit@live.dk>
2017-10-13 15:39:19 +0200

    Navbar changed colors, with new maincontent area added an frontpage image.

--- index.php
+++ index.php
@@ -4,17 +4,18 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Own made CSS -->
-	<link rel="stylesheet" href="bin/css/mainStyle.css">
 	
     <!-- Bootstrap CSS AND SCRIPTS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	
+  
+	<!-- Own made CSS -->
+	<link href="bin/css/mainStyle.css" rel="stylesheet">
   
   </head>
   <body>
@@ -31,7 +32,7 @@
 	</div>
 	
 	<!-- Menu bar for navigation -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light">
 		<div class="container">
 		<a class="brand" href="#"></a>
 		  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,31 +41,39 @@
 		  <div class="collapse navbar-collapse" id="navbarNavDropdown">
 			<ul class="navbar-nav">
 			  <li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="#">Forside <span class="sr-only">(current)</span></a>
 			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="#">Features</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="#">Pricing</a>
-			  </li>
-			  <li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				  Dropdown link
-				</a>
+			
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Hold</a>
 				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 				  <a class="dropdown-item" href="#">Action</a>
 				  <a class="dropdown-item" href="#">Another action</a>
 				  <a class="dropdown-item" href="#">Something else here</a>
 				</div>
 			  </li>
+			
+			
+			  <li class="nav-item"><a class="nav-link" href="#">Klubben</a></li>
+			  <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
+			  <li class="nav-item"><a class="nav-link" href="#">Sponsorer</a></li>
+			  <li class="nav-item"><a class="nav-link" href="#">Kampe/resultater</a></li>
+			 
+			 
+			  
+			  
 			</ul>
 		  </div>
 		</div>
 	</nav>
 	
 	<!-- Page content -->
-	
+	<div class="container" id="mainContentImage">
+		<img src="https://stillmed.olympic.org/media/Images/OlympicOrg/News/2016/08/20/2016-08-20-Handball-Women-thumbnail.jpg" width="100%">
+	</div>
+	<div class="container" id="mainContent">
+	hej
+	</div>
 	
 
     <!-- Optional JavaScript -->
